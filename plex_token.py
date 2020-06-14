114bc3fba4f3f7bf7bf4b51d4cbc509f5e626a20
Christian Widlund <christian.widlund@gmail.com>
2020-06-14 14:24:40 +0200

    Use Uppercase Constants

--- plex_token.py
+++ plex_token.py
@@ -3,31 +3,34 @@
 
 import base64
 import http.client
-import urllib.error
 import urllib.request
+
 import user_info
 
-base64string = base64.encodebytes(
-    ('%s:%s' % (user_info.plex_username, user_info.plex_password)).encode()).decode().replace('\n', '')
-txdata = ""
+BASE_64_STRING = base64.encodebytes(
+    ('%s:%s' % (user_info.plex_username, user_info.plex_password)).encode()
+).decode().replace('\n', '')
+TX_DATA = ""
 
-headers = {'Authorization': "Basic %s" % base64string,
+headers = {'Authorization': "Basic %s" % BASE_64_STRING,
            'X-Plex-Client-Identifier': "MMA script",
            'X-Plex-Product': "MMA script 356546545",
            'X-Plex-Version': "0.001"}
 
-conn = http.client.HTTPSConnection("plex.tv")
-conn.request("POST", "/users/sign_in.json", txdata, headers)
-response = conn.getresponse()
+CONN = http.client.HTTPSConnection("plex.tv")
+CONN.request("POST", "/users/sign_in.json", TX_DATA, headers)
+response = CONN.getresponse()
 data = response.read()
-data_str = str(data)
-token_str_plus = data_str.split('_token":"')[1]
+DATA_STR = str(data)
+token_str_plus = DATA_STR.split('_token":"')[1]
 token_str = token_str_plus.split('"')[0]
 token = token_str
-conn.close()
+CONN.close()
 
 section_info_xml = urllib.request.urlopen(
-    'http://' + user_info.plex_ip + ':32400/library/sections/?X-Plex-Token=' + token_str).read().decode('utf-8')
+    'http://' + user_info.plex_ip + ':32400/library/sections/?X-Plex-Token=' + token_str
+).read().decode('utf-8')
+
 section_info_plus2 = section_info_xml.split(user_info.mma_lib)[0]
 section_info_plus = section_info_plus2.rsplit('key="', 1)[1]
 section_str = section_info_plus.split('"')[0]
