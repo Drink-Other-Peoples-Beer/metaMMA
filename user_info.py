ac982f0cdda8ca163ab93da28fca519fb0080757
Christian Widlund <christian.widlund@gmail.com>
2020-07-08 21:31:28 +0200

    use env files instead

--- user_info.py
+++ user_info.py
@@ -1,30 +1,33 @@
-#!/usr/bin/env python
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MMA = getenv("MMA")
+MMA_DESTINATION = getenv("MMA_DESTINATION")
+UFC = getenv("UFC")
+UFC_DESTINATION = getenv("UFC_DESTINATION")
+BELLATOR = getenv("BELLATOR")
+BEL_DESTINATION = getenv("BEL_DESTINATION")
+INVICTA = getenv("INVICTA")
+INV_DESTINATION = getenv("INV_DESTINATION")
+WSOF = getenv("WSOF")
+WSOF_DESTINATION = getenv("WSOF_DESTINATION")
+TITAN = getenv("TITAN")
+TTN_DESTINATION = getenv("TTN_DESTINATION")
+LEGACY = getenv("LEGACY")
+LFA_DESTINATION = getenv("LFA_DESTINATION")
+ONE = getenv("ONE")
+ONE_DESTINATION = getenv("ONE_DESTINATION")
+GLORY = getenv("GLORY")
+GLR_DESTINATION = getenv("GLR_DESTINATION")
 # ______________________________________________________________
-MMA = 2
-mma_destination = '/media/QQQ/MMA/'
-UFC = 2
-ufc_destination = '/media/QQQ/UFC/'
-Bellator = 2
-bel_destination = '/media/QQQ/Bellator/'
-Invicta = 2
-inv_destination = '/media/QQQ/Invicta/'
-WSOF = 2
-wsof_destination = '/media/QQQ/WSOF/'
-Titan = 2
-ttn_destination = '/media/QQQ/Titan FC/'
-Legacy = 2
-lfa_destination = '/media/QQQ/Legacy/'
-ONE = 2
-one_destination = '/media/QQQ/One/'
-Glory = 2
-glr_destination = '/media/QQQ/Glory/'
-# ______________________________________________________________
-tmp_dir = '/media/QQQ/tmp/'
-done_dir = '/media/QQQ/done/'
-refresh_plex = 1
-plex_username = 'QQQ'
-plex_password = 'QQQ'
-plex_ip = '192.168.QQQ.QQQ'
-refresh_kodi = 0
-mma_lib = 'QQQ'
-# ______________________________________________________________
+TMP_DIR = getenv("TMP_DIR")
+DONE_DIR = getenv("DONE_DIR")
+REFRESH_PLEX = getenv("REFRESH_PLEX")
+PLEX_USERNAME = getenv("PLEX_USERNAME")
+PLEX_PASSWORD = getenv("PLEX_PASSWORD")
+PLEX_IP = getenv("PLEX_IP")
+REFRESH_KODI = getenv("REFRESH_KODI", 0)
+MMA_LIB = getenv("MMA_LIB")
